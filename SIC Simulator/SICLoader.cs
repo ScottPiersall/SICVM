cec939dbc6f6ea08631f3c642924d417184043c5
Scott Piersall <scottp@ccscorp.biz>
2020-10-26 19:45:07 -0400

    Added Loader Class Framework. Assigned to Brandon

---
+++ SIC Simulator/SICLoader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIC_Simulator
+{
+    // Assigned to Brandom Woodrum
+
+    // This class should have two constructors
+    // Zero Parameter (Absolute)
+    // Single Parameter (StartAddress) - > Relocate program to StartAddress
+
+    class SICLoader
+    {
+ 
+    
+    
+    }
+}
