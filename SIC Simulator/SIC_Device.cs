edb6c12d5b4de1331b1bc925cf103f0752479b56
Scott Piersall <scottp@ccscorp.biz>
2020-10-28 20:12:51 -0400

    Added Additional Methods to SIC_DEVICE Class

--- SIC Simulator/SIC_Device.cs
+++ SIC Simulator/SIC_Device.cs
@@ -23,5 +23,28 @@ namespace SIC_Simulator
             WriteBuffer.Add(Value);
         }
 
+
+        /// <summary>
+        /// Returns a UTF-8 String of the ASCII Bytes written to this device
+        /// </summary>
+        /// <returns></returns>
+        public string GetASCIIStringWrites()
+        {
+            String Result = String.Empty;
+            return Result;
+        }
+
+        /// <summary>
+        /// Returns a UTF-8 String of the HEX Codes of the bytes written to this device
+        /// </summary>
+        /// <returns></returns>
+        public string GetHEXStringWrites()
+        {
+            String Result = String.Empty;
+            return Result;
+        }
+
+
+
     }
 }
