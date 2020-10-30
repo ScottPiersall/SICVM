20b3634a38645d528764b20278dc278aa864410f
Scott Piersall <spiersall@gmail.com>
2020-10-30 08:56:01 -0400

    Updated Assembly Infor to track version number. Update About to show contributors and Roles and version number from Assembly

--- SIC Simulator/frmAbout.cs
+++ SIC Simulator/frmAbout.cs
@@ -15,7 +15,33 @@ namespace SIC_Simulator
         public frmAbout()
         {
             InitializeComponent();
+
+     
         }
+
+
+        private void frmAbout_Load(object sender, EventArgs e)
+        {
+            ListViewItem ScottPLvItem;
+            ScottPLvItem = new ListViewItem("Scott Piersall", "Chief Architect & Lead Developer VM");
+            this.lvAuthors.Items.Add(ScottPLvItem);
+
+            ListViewItem RileySLvItem;
+            RileySLvItem = new ListViewItem("Riley Strickland", "Pass 1 & 2 of SIC Assembler");
+            this.lvAuthors.Items.Add(RileySLvItem);
+
+            ListViewItem EllisLLvItem;
+            EllisLLvItem = new ListViewItem("Ellis Levine", "Pass 1 & 2 of SIC Assembler");
+            this.lvAuthors.Items.Add(EllisLLvItem);
+
+            ListViewItem BrandonWLvItem;
+            BrandonWLvItem = new ListViewItem("Brandon Woodrum", "Asbolute & Relocating Loader");
+            this.lvAuthors.Items.Add(BrandonWLvItem);
+
+
+            this.lblVersion.Text = "Version: " + Application.ProductVersion.ToString();
+        }
+
 
         private void btnOk_Click(object sender, EventArgs e)
         {
