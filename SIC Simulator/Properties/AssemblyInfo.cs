d322fd92b48a11af51daf985e949850e86d9f340
Scott Piersall <spiersall@gmail.com>
2020-10-31 14:35:23 -0400

    Bumped Release Version to 20.10.31.0

--- SIC Simulator/Properties/AssemblyInfo.cs
+++ SIC Simulator/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("20.10.30.1")]
-[assembly: AssemblyFileVersion("20.10.30.1")]
+[assembly: AssemblyVersion("20.10.31.0")]
+[assembly: AssemblyFileVersion("20.10.31.0")]
