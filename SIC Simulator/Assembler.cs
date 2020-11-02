f8fef7dbe870731608b09cbe7ff8295937c43f24
Kris Wieben <kwieben221@gmail.com>
2020-11-02 16:57:18 -0500

    added a regex to help correct some malformed sic source

--- SIC Simulator/Assembler.cs
+++ SIC Simulator/Assembler.cs
@@ -93,7 +93,7 @@ namespace SIC_Simulator
             String line;
             while ((line = file.ReadLine()) != null)
             {
-                line = Regex.Replace(line, @"([ ]+)", "\t"); // thank you J for posting your CLOCK code = ) .. all spaces are replaced with tabs now
+                line = Regex.Replace(line, @"([ ]|[\t]{2,})+", "\t"); // thank you J for posting your CLOCK code = ) .. all spaces are replaced with tabs now
                 line.TrimEnd();
                 line_counter++;
                 if (line[0] == 35 || line.Length == 0) // skip comments and blank lines
