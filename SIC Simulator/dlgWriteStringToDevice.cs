8460d583a1ca6e61d2d381b1b5f06745299fed67
Dylan Strickley <Dylans17@users.noreply.github.com>
2021-12-06 23:09:32 -0500

    Merge

--- SIC Simulator/dlgWriteStringToDevice.cs
+++ SIC Simulator/dlgWriteStringToDevice.cs
@@ -15,8 +15,6 @@ namespace SIC_Simulator
         
         public string result = string.Empty;
 
-        public string result = string.Empty;
-
         public dlgWriteStringToDevice()
         {
             InitializeComponent();
@@ -25,12 +23,6 @@ namespace SIC_Simulator
 
         private void button1_Click(object sender, EventArgs e)
         {
-            String temp = this.textWord.Text.Trim();
-            int TempW;
-            temp = this.textWord.Text.Trim();
-            TempW = int.Parse(temp);
-
-            this.result = this.textWord.Text;
             this.DialogResult = DialogResult.OK;
             this.result = this.textBox.Text;
         }
